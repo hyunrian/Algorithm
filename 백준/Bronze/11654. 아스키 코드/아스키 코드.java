47500ce175ee99aab47ea53caa0a341de914897a
HyunRian <121999215+hyunrian@users.noreply.github.com>
2024-02-02 18:28:45 +0900

    [Bronze V] Title: 아스키 코드, Time: 200 ms, Memory: 17608 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        char ch = scan.next().charAt(0);
+        System.out.println(Integer.valueOf(ch));
+    }
+}
