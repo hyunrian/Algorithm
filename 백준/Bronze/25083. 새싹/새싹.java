0ef1e74da4d88a48316d30d24d5d42023d377f5f
HyunRian <121999215+hyunrian@users.noreply.github.com>
2024-02-03 17:01:10 +0900

    [Bronze V] Title: 새싹, Time: 116 ms, Memory: 13900 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("         ,r'\"7\nr`-_   ,'  ,/\n \\. \". L_r'\n   `~\\/\n      |\n      |");
+        System.out.println(sb);
+    }
+}
