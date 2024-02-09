90a439817a7253de097d150786b26e4907645abe
HyunRian <121999215+hyunrian@users.noreply.github.com>
2024-02-09 15:51:21 +0900

    [Silver V] Title: 팩토리얼 0의 개수, Time: 216 ms, Memory: 18124 KB -BaekjoonHub

---
+++ 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.java
@@ -0,0 +1,24 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        BigInteger big = new BigInteger("1");
+
+        for (int i = 1; i <= n; i++) {
+            big = big.multiply(BigInteger.valueOf(i));
+        }
+
+        String s = String.valueOf(big);
+        int count = 0;
+
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s.charAt(i) == '0') count++;
+            else break;
+        }
+        System.out.println(count);
+    }
+}
