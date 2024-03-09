a55d7d8107a77036e5fce9831b78ee1ecbdc56e2
HyunRian <121999215+hyunrian@users.noreply.github.com>
2024-03-09 15:12:32 +0900

    [Silver II] Title: 골드바흐 파티션, Time: 268 ms, Memory: 17860 KB -BaekjoonHub

--- 백준/Silver/17103. 골드바흐 파티션/골드바흐 파티션.java
+++ 백준/Silver/17103. 골드바흐 파티션/골드바흐 파티션.java
@@ -1,44 +1,33 @@
 import java.io.*;
-import java.util.LinkedHashSet;
 
 public class Main {
-    static LinkedHashSet<Integer> primes = new LinkedHashSet<>();
+    static boolean[] arr = new boolean[1000001];
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
 
+        makePrime();
         int T = Integer.parseInt(br.readLine());
 
         while (T-- > 0) {
             int N = Integer.parseInt(br.readLine());
-            makePrime(N);
-            sb.append(getCount(N) + "\n");
+            int count = 0;
+            for (int i = 2; i <= N / 2; i++) {
+                if (!arr[i] && !arr[N - i]) count++;
+            }
+            sb.append(count + "\n");
         }
         System.out.println(sb);
     }
 
-    static void makePrime(int N) {
-        boolean[] arr = new boolean[N]; // 0 ~ N-1
+    static void makePrime() {
         arr[0] = arr[1] = true;
 
-        for (int i = 2; i < Math.sqrt(N); i++) {
+        for (int i = 2; i <= Math.sqrt(arr.length - 1); i++) {
             if (arr[i]) continue;
             for (int j = i * i; j < arr.length; j += i) {
                 arr[j] = true;
             }
         }
-        for (int i = 2; i < arr.length; i++) {
-            if (!arr[i]) primes.add(i);
-        }
-    }
-
-    static int getCount(int N) {
-        int count = 0;
-
-        for (Integer a : primes) {
-            int b = N - a;
-            if (primes.contains(b) && b >= a) count++;
-        }
-        return count;
     }
 }
